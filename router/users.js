7359db52c44d432678537aa49e4115b365b76c3d
hilmi <hilmi.hilmiramadani2110@gmail.com>
2023-11-07 21:23:31 +0700

    Koneksi database mongoDB

--- router/users.js
+++ router/users.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
-const usercontroller = require('../controllers/user')
+const usercontroller = require('../controllers/users')
 
 router.get('/users', usercontroller.index)
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const userrouter = require('./router/users')
+const userRouter = require('./router/users')
 const port = 3000
+const connectDB = require('./config/db')
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -10,8 +11,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use(userrouter)
+app.use(userRouter)
 
+connectDB()
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
