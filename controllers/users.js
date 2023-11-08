4901fa634eb308834763ee323cd0992d24a9d466
hilmi <hilmi.hilmiramadani2110@gmail.com>
2023-11-08 13:43:43 +0700

    Menambahkan data

--- controllers/users.js
+++ controllers/users.js
@@ -22,15 +22,21 @@ module.exports = {
         }
 
     },
-    store: (req, res) => {
-        users.push(req.body)
-        res.json({
-            status: true,
-            data: users,
-            method: req.method,
-            url: req.url,
-            message: "Data berhasil ditambahkan!"
-        })
+    store: async (req, res) => {
+        try {
+            const user = await User.create(req.body)
+            res.status(200).json({
+                status: true,
+                data: user,
+                method: req.method,
+                url: req.url,
+                message: "Data berhasil ditambahkan!"
+            })
+        } catch (error) {
+
+        }
+        // users.push(req.body)
+
     },
     update: (req, res) => {
         const id = req.params.id
