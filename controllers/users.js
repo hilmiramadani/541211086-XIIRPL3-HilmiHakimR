24be2d8077d14a36a904773b18bbc72c14def28d
hilmi <hilmi.hilmiramadani2110@gmail.com>
2023-11-08 08:49:17 +0700

    Menampilkan semua data

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,26 @@
 const User = require('../models/User')
 
 module.exports = {
-    index: (req, res) => {
-        if (users.length > 0) {
-            res.json({
-                status: true,
-                data: users,
-                method: req.method,
-                url: req.url
-            })
-        } else {
-            res.json({
-                status: false,
-                message: "Data masih kosong"
-            })
+    index: async (req, res) => {
+        try {
+            const users = await User.find()
+            if (users.length > 0) {
+                res.status(200).json({
+                    status: true,
+                    data: users,
+                    method: req.method,
+                    url: req.url
+                })
+            } else {
+                res.json({
+                    status: false,
+                    message: "Data masih kosong"
+                })
+            }
+        } catch (error) {
+            res.status(400).json({ sucess: false })
         }
+
     },
     store: (req, res) => {
         users.push(req.body)
